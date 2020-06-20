d22b0a159ef0824e05f5369b528688f172e42be8
Yocoyani <yocoyaniperez1@gmail.com>
2020-06-20 14:06:21 -0500

    Clase 2

---
+++ ListasClase2.py
@@ -0,0 +1,57 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jun 20 12:05:07 2020
+
+@author: yocoy
+"""
+
+"""
+Estructuras
+
+"""
+#Listas
+
+x = [0,7,8,9,3]
+# print(x)
+# print(x[1])
+
+# x.append(3)
+# print(x)
+
+# x.remove(3)
+# print(x)
+
+# palabras = ["Hola","Mundo","Quien", "sabe"]
+# print(palabras)
+
+# lista = [0, "Python", 0.76, "Hola",x, [0,1,2,3]]
+
+# print(lista)
+
+# print(lista[1],lista[4])
+
+# lista.insert(2,3)
+
+# print(lista)
+
+# y = [i for i in range(1000)]
+# #print(y)
+
+# print(y.index(555))
+
+# print(lista.index(0.76))
+
+# # lista.pop()
+# print(lista)
+
+# lista.clear()
+# print(lista)
+
+for i in x:
+    if i == 8:
+        print("Encontre el 8 dentro de la lista")
+        
+print(len(x))
+        
+
+    
